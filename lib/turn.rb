93aa690b1b335693104922628b2f12d5def2a361
Emily Jirles <emily.jirles@gmail.com>
2018-04-24 15:40:40 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,3 +33,15 @@ def position_taken?(board, index)
     true 
   end
 end
+
+
+def turn 
+  #ask for input
+#get input
+#convert input to index
+#if index is valid
+ # make the move for index
+ # show the board
+#else
+ # ask for input again until you get a valid input
+end
